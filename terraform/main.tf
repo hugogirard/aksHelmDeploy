7dc0e2902bc0f44af6ce85dd084a6d45b7c4a183
Hugo Girard <hugogirard28@hotmail.com>
2021-01-07 21:58:11 -0500

    fix subnet in vnet

--- terraform/main.tf
+++ terraform/main.tf
@@ -34,52 +34,52 @@ module "jumpbox" {
   subnet_id = azurerm_subnet.jumpBoxSubnet.id
 }
 
-resource "azurerm_kubernetes_cluster" "aks" {
-  name                = "${var.prefix}-k8s"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  dns_prefix          = "${var.prefix}-k8s"
+# resource "azurerm_kubernetes_cluster" "aks" {
+#   name                = "${var.prefix}-k8s"
+#   location            = azurerm_resource_group.rg.location
+#   resource_group_name = azurerm_resource_group.rg.name
+#   dns_prefix          = "${var.prefix}-k8s"
 
-  default_node_pool {
-    name       = "default"
-    node_count = 1
-    vm_size    = "Standard_DS2_v2"
-    vnet_subnet_id = azurerm_subnet.aksSubnet.id
-  }
+#   default_node_pool {
+#     name       = "default"
+#     node_count = 1
+#     vm_size    = "Standard_DS2_v2"
+#     vnet_subnet_id = azurerm_subnet.aksSubnet.id
+#   }
 
-  network_profile {
-    network_plugin = "azure"
-    load_balancer_sku = "standard"
-    docker_bridge_cidr = var.network_docker_bridge_cidr
-    dns_service_ip     = var.network_dns_service_ip
-    service_cidr       = var.network_service_cidr    
-  }
+#   network_profile {
+#     network_plugin = "azure"
+#     load_balancer_sku = "standard"
+#     docker_bridge_cidr = var.network_docker_bridge_cidr
+#     dns_service_ip     = var.network_dns_service_ip
+#     service_cidr       = var.network_service_cidr    
+#   }
 
-  identity {
-    type = "SystemAssigned"
-  }
+#   identity {
+#     type = "SystemAssigned"
+#   }
 
-  private_cluster_enabled = true
+#   private_cluster_enabled = true
 
-  addon_profile {
-    aci_connector_linux {
-      enabled = false
-    }
+#   addon_profile {
+#     aci_connector_linux {
+#       enabled = false
+#     }
 
-    azure_policy {
-      enabled = false
-    }
+#     azure_policy {
+#       enabled = false
+#     }
 
-    http_application_routing {
-      enabled = false
-    }
+#     http_application_routing {
+#       enabled = false
+#     }
 
-    kube_dashboard {
-      enabled = true
-    }
+#     kube_dashboard {
+#       enabled = true
+#     }
 
-    oms_agent {
-      enabled = false
-    }
-  }
-}
+#     oms_agent {
+#       enabled = false
+#     }
+#   }
+# }
