c74d6eae7258dc6fc8a0cbabbbf327100f89baff
Hugo Girard <hugogirard28@hotmail.com>
2021-01-08 16:59:36 -0500

    fix acr

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,7 +19,7 @@ resource "azurerm_subnet" "aksSubnet" {
 
 module "acr" {
   source              = "./modules/containerRegistry"
-  
+  prefix              = var.prefix
 }
 
 resource "azurerm_kubernetes_cluster" "aks" {
