69e3f3daf904f9dceb48751988397d7cd8b08903
Hugo Girard <hugogirard28@hotmail.com>
2021-01-09 14:28:59 -0500

    added the prefix variable in sql

--- terraform/modules/sql/variables.tf
+++ terraform/modules/sql/variables.tf
@@ -2,6 +2,10 @@ variable "rgName" {
     description = "The name of the resource group"
 }
 
+variable "prefix" {
+    description = "A prefix used for all resources in this example"
+}
+
 variable "location" {
   description = "The Azure Region in which all resources in this example should be provisioned"
 }
