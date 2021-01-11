8d4835d64ceac25eb6f3d3df9d450f058f37799d
Hugo Girard <hugogirard28@hotmail.com>
2021-01-11 13:52:53 -0500

    fix container

--- src/TodoApi/Startup.cs
+++ src/TodoApi/Startup.cs
@@ -36,7 +36,7 @@ namespace TodoApi
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
-            {
+            {                
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "TodoApi", Version = "v1" });
             });
         }
@@ -44,15 +44,15 @@ namespace TodoApi
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => 
             {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "TodoApi v1"));
-            }
-
-            app.UseHttpsRedirection();
-
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "TodoApi v1");
+                c.RoutePrefix = string.Empty;
+            });
+            
             app.UseRouting();
 
             app.UseAuthorization();
